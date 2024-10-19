b7e670bdb3adc20662890c3c13324376b8e31ef0
Daniel Souza Scremim <danielsouzascremim@hotmail.com>
2024-10-19 20:01:28 -0300

    Subindo o arquivo com o desafio realizado exatamente como o solicitado no enunciado do desafio.

---
+++ controle_fluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package controle_fluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
